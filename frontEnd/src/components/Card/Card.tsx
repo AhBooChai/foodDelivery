a0f75a8a8954063a33e30521d6849ee3aac6df92
Manel Ariapala <48101886+AhBooChai@users.noreply.github.com>
2025-03-03 17:38:28 +0100

    Install axios fetch data create menu page

--- frontEnd/src/components/Card/Card.tsx
+++ frontEnd/src/components/Card/Card.tsx
@@ -5,15 +5,14 @@ interface CardProps {
   title: string;
   price: number;
   id?: number;
-  category: "food" | "beverages";
 }
-const Card = ({ imgSrc, title, price, id, category }: CardProps) => {
+const Card = ({ imgSrc, title, price, id }: CardProps) => {
   return (
     <div className="card" key={id}>
       <img src={imgSrc} alt={title} className="card__img" />
       <h3 className="card__title">{title}</h3>
       <p>{price}</p>
-      <p>{category}</p>
+      <p></p>
       <button className="btn-circle btn-x card-btn">
         <span className="visually-hidden"></span>
       </button>
--- frontEnd/src/pages/Menu/Menu.tsx
+++ frontEnd/src/pages/Menu/Menu.tsx
@@ -1,6 +1,64 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 import "./Menu.scss";
+import Card from "../../components/Card";
+
+interface Product {
+  id: number;
+  category?: string;
+  img: string;
+  name: string;
+  price: number;
+}
+
 const Menu = () => {
-  return <div>menu</div>;
+  const [products, setProducts] = useState<Product[]>([]);
+  const [food, setFood] = useState<Product[]>([]);
+  const [drinks, setDrinks] = useState<Product[]>([]);
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get(
+          "https://food-delivery-app-five-gamma.vercel.app/api/dishes"
+        );
+        console.log(response.data);
+        setProducts(response.data);
+        setFood(
+          response.data.filter((item: Product) => item.category === "food")
+        );
+        setDrinks(
+          response.data.filter((item: Product) => item.category === "beverages")
+        );
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+        console.log("Something went wrong");
+        setLoading(false);
+      }
+    }
+    fetchData();
+  }, []);
+
+  return loading ? (
+    <p>Loading..........</p>
+  ) : (
+    <div className="menu__container">
+      <h1 className="menu__title">Menu</h1>
+      <div className="menu__grid">
+        {products.map((product) => (
+          <Card
+            key={product.id}
+            imgSrc={product.img}
+            title={product.name}
+            price={product.price}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Menu;
