5d929ca8db04a07668f1c2ababada36a69d2d0bb
Manel Ariapala <48101886+AhBooChai@users.noreply.github.com>
2025-04-01 08:44:09 +0200

    Refactoring of Menu component by accessing ProductContext for product info

--- frontEnd/src/pages/Menu/Menu.tsx
+++ frontEnd/src/pages/Menu/Menu.tsx
@@ -1,65 +1,17 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
+import { useProducts } from "../../context/ProductsContext";
 import "./Menu.scss";
 import Card from "../../components/Card";
 
-interface Product {
-  id: number;
-  category?: string;
-  img: string;
-  name: string;
-  price: number;
-}
-
 const Menu = () => {
-  const [products, setProducts] = useState<Product[]>([]);
-  const [filteredProducts, setFilteredProducts] = useState<Product[]>([]);
-  const [search, setSearch] = useState("");
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const response = await axios.get(
-          "https://food-delivery-app-five-gamma.vercel.app/api/dishes"
-        );
-        console.log(response.data);
-        setProducts(response.data);
-        setFilteredProducts(response.data);
-        setLoading(false);
-      } catch (error) {
-        console.log(error);
-        console.log("Something went wrong");
-        setLoading(false);
-      }
-    }
-    fetchData();
-  }, []);
-
-  const showFood = () => {
-    setFilteredProducts(products.filter((item) => item.category === "food"));
-  };
-
-  const showDrinks = () => {
-    setFilteredProducts(
-      products.filter((item) => item.category === "beverages")
-    );
-  };
-
-  const showAll = () => {
-    setFilteredProducts(products);
-  };
-
-  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const value = event.target.value.toLowerCase();
-    setSearch(value);
-
-    const filtered = products.filter((product) =>
-      product.name.toLowerCase().includes(value)
-    );
-
-    setFilteredProducts(filtered);
-  };
+  const {
+    filteredProducts,
+    search,
+    loading,
+    showFood,
+    showDrinks,
+    showAll,
+    handleSearch,
+  } = useProducts();
 
   return loading ? (
     <p>Loading..........</p>
---
+++ frontEnd/src/pages/Menu2/Menu2.tsx
@@ -0,0 +1,7 @@
+import "./Menu2.scss";
+
+const Menu2 = () => {
+  return <div></div>;
+};
+
+export default Menu2;
