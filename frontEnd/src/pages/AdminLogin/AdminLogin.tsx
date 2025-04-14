f32a70d0a67d210b7bdb4b6e46bf211029c697ab
Manel Ariapala <48101886+AhBooChai@users.noreply.github.com>
2025-04-14 14:19:29 +0200

    Create AdminLogin page

--- frontEnd/src/pages/AdminLogin/AdminLogin.tsx
+++ frontEnd/src/pages/AdminLogin/AdminLogin.tsx
@@ -1,7 +1,46 @@
+import { useState } from "react";
+import { useAuth } from "../../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 import "./AdminLogin.scss";
 
 const AdminLogin = () => {
-  return <div></div>;
+  const { login } = useAuth();
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    const success = login(email, password);
+
+    if (success) {
+      navigate("/admin/add-item");
+    } else {
+      alert("Invalid credentials");
+    }
+  };
+  return (
+    <div className="login__container">
+      <h2>Admin Login</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="admin@example.com"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="admin123"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <button type="submit">Log In</button>
+      </form>
+    </div>
+  );
 };
 
 export default AdminLogin;
