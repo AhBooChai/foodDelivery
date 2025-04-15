342cdd121d44336cd30ed3677d2d2b0cd30c66bd
Manel Ariapala <48101886+AhBooChai@users.noreply.github.com>
2025-04-15 10:01:22 +0200

    Refactor Footer component to display items in the center

--- frontEnd/src/components/Footer/Footer.tsx
+++ frontEnd/src/components/Footer/Footer.tsx
@@ -1,9 +1,19 @@
+import { Link } from "react-router-dom";
 import "./Footer.scss";
 
 const Footer = () => {
   const d = new Date();
   const year = d.getFullYear();
-  return <footer>© {year} Copyright by AtHomeFineDining.com</footer>;
+  return (
+    <footer>
+      <div className="container">
+        © {year} Copyright by AtHomeFineDining.com
+        <Link to="/admin-login" className="footer__admin-link">
+          Admin Login
+        </Link>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
